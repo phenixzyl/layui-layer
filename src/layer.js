0361fce81b821f614c52cd1278ad1012f449b80a
sight <1453017105@qq.com>
2023-05-17 00:16:57 +0800

    feat: 同步 layui 2.8.3

--- src/layer.js
+++ src/layer.js
@@ -98,7 +98,7 @@ var isLayui = window.layui && layui.define, $, win, ready = {
 
 //默认内置方法。
 export var layer = {
-  v: '2.8.2',
+  v: '2.8.3',
   ie: function(){ //ie 版本
     var agent = navigator.userAgent.toLowerCase();
     return (!!window.ActiveXObject || "ActiveXObject" in window) ? (
@@ -256,7 +256,7 @@ doms.anim = {
 doms.SHADE = 'layui-layer-shade';
 doms.MOVE = 'layui-layer-move';
 
-//默认配置
+// 默认配置
 Class.pt.config = {
   type: 0,
   shade: 0.3,
@@ -266,16 +266,16 @@ Class.pt.config = {
   offset: 'auto',
   area: 'auto',
   closeBtn: 1,
-  time: 0, //0表示不自动关闭
+  icon: -1,
+  time: 0, // 0 表示不自动关闭
   zIndex: 19891014, 
   maxWidth: 360,
   anim: 0,
-  isOutAnim: true, //退出动画
-  minStack: true, //最小化堆叠
-  icon: -1,
+  isOutAnim: true, // 退出动画
+  minStack: true, // 最小化堆叠
   moveType: 1,
   resize: true,
-  scrollbar: true, //是否允许浏览器滚动条
+  scrollbar: true, // 是否允许浏览器滚动条
   tips: 2
 };
 
@@ -383,25 +383,45 @@ Class.pt.vessel = function(conType, callback){
   return that;
 };
 
-//创建骨架
+// 创建骨架
 Class.pt.creat = function(){
-  var that = this
-  ,config = that.config
-  ,times = that.index, nodeIndex
-  ,content = config.content
-  ,conType = typeof content === 'object'
-  ,body = $('body');
+  var that = this;
+  var config = that.config;
+  var times = that.index, nodeIndex;
+  var content = config.content;
+  var conType = typeof content === 'object';
+  var body = $('body');
   
-  if(config.id && $('.'+ doms[0]).find('#'+ config.id)[0]) return;
+  // 若 id 对应的弹层已经存在，则不重新创建
+  if(config.id && $('.'+ doms[0]).find('#'+ config.id)[0]){
+    return (function(){
+      var layero = $('#'+ config.id).closest('.'+ doms[0]);
+      var index = layero.attr('times');
+      var options = layero.data('config');
+      var elemShade = $('#'+ doms.SHADE + index);
+      
+      var maxminStatus = layero.data('maxminStatus') || {};
+      // 若弹层为最小化状态，则点击目标元素时，自动还原
+      if(maxminStatus === 'min'){
+        layer.restore(index);
+      } else if(options.hideOnClose){
+        elemShade.show();
+        layero.show();
+      };
+    })();
+  };
+
+  // 是否移除活动元素的焦点
   if(config.removeFocus) {
     document.activeElement.blur(); // 将原始的聚焦节点失焦
   }
 
+  // 初始化 area 属性
   if(typeof config.area === 'string'){
     config.area = config.area === 'auto' ? ['', ''] : [config.area, ''];
   }
   
-  //anim兼容旧版shift
+  // anim 兼容旧版 shift
   if(config.shift){
     config.anim = config.shift;
   }
@@ -501,13 +521,8 @@ Class.pt.creat = function(){
     });
   };
   
-  // 记录关闭动画
-  if(config.isOutAnim){
-    that.layero.data({
-      isOutAnim: true,
-      anim: config.anim
-    });
-  }
+  // 记录配置信息
+  that.layero.data('config', config);
 };
 
 // 当前实例的 resize 事件
@@ -1026,8 +1041,6 @@ layer.style = function(index, options, limit){
 
 // 最小化
 layer.min = function(index, options){
-  options = options || {};
-
   var layero = $('#'+ doms[0] + index);
   var maxminStatus = layero.data('maxminStatus');
 
@@ -1035,8 +1048,10 @@ layer.min = function(index, options){
   if(maxminStatus === 'max') layer.restore(index); // 若当前为最大化，则先还原后再最小化
 
   layero.data('maxminStatus', 'min');
+  options = options || layero.data('config') || {};
 
   var shadeo = $('#'+ doms.SHADE + index);
+  var elemMin = layero.find('.layui-layer-min');
   var titHeight = layero.find(doms[1]).outerHeight() || 0;
   var minLeft = layero.attr('minLeft'); // 最小化时的横坐标
   var hasMinLeft = typeof minLeft === 'string'; // 是否已经赋值过最小化坐标
@@ -1078,7 +1093,7 @@ layer.min = function(index, options){
   layero.attr('position', position);
   layer.style(index, settings, true);
 
-  layero.find('.layui-layer-min').hide();
+  elemMin.hide();
   layero.attr('type') === 'page' && layero.find(doms[4]).hide();
   ready.rescollbar(index);
 
@@ -1158,7 +1173,10 @@ layer.close = function(index, callback){
     ) : $('#'+ doms[0] + index)
   }();
   var type = layero.attr('type');
-  var data = layero.data() || {};
+  var options = layero.data('config') || {};
+  var hideOnClose = options.id && options.hideOnClose; // 是否关闭时移除弹层容器
+
+  if(!layero[0]) return;
 
   // 关闭动画
   var closeAnim = ({
@@ -1166,11 +1184,19 @@ layer.close = function(index, callback){
     slideLeft: 'layer-anim-slide-left-out',
     slideUp: 'layer-anim-slide-up-out',
     slideRight: 'layer-anim-slide-right-out'
-  })[data.anim] || 'layer-anim-close';
-  
-  if(!layero[0]) return;
-  
-  var WRAP = 'layui-layer-wrap', remove = function(){
+  })[options.anim] || 'layer-anim-close';
+
+  // 移除主容器
+  var remove = function(){
+    var WRAP = 'layui-layer-wrap';
+
+    // 是否关闭时隐藏弹层容器
+    if(hideOnClose){
+      layero.removeClass('layer-anim '+ closeAnim);
+      return layero.hide();
+    }
+
+    // 是否为页面捕获层
     if(type === ready.type[1] && layero.attr('conType') === 'object'){
       layero.children(':not(.'+ doms[5] +')').remove();
       var wrap = layero.find('.'+WRAP);
@@ -1179,7 +1205,7 @@ layer.close = function(index, callback){
       }
       wrap.css('display', wrap.data('display')).removeClass(WRAP);
     } else {
-      //低版本IE 回收 iframe
+      // 低版本 IE 回收 iframe
       if(type === ready.type[2]){
         try {
           var iframe = $('#'+ doms[4] + index)[0];
@@ -1202,13 +1228,18 @@ layer.close = function(index, callback){
       delete ready.events.resize[index];
     }
   };
+  // 移除遮罩
+  var removeShade = (function fn(){
+    $('#'+ doms.SHADE + index)[
+      hideOnClose ? 'hide' : 'remove'
+    ]();
+  })();
   
   // 是否允许关闭动画
-  if(data.isOutAnim){
+  if(options.isOutAnim){
     layero.addClass('layer-anim '+ closeAnim);
   }
   
-  $('#layui-layer-moves, #'+ doms.SHADE + index).remove();
   layer.ie == 6 && ready.reselect();
   ready.rescollbar(index); 
   
@@ -1218,7 +1249,7 @@ layer.close = function(index, callback){
     ready.minStackArr.push(layero.attr('minLeft'));
   }
   
-  if((layer.ie && layer.ie < 10) || !layero.data('isOutAnim')){
+  if((layer.ie && layer.ie < 10) || !options.isOutAnim){
     remove()
   } else {
     setTimeout(function(){
